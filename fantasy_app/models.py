1c161235d0cf20cb01eee37d1b78e6868b6ab9ec
Justin <41461549+justin-hsieh@users.noreply.github.com>
2022-09-27 08:59:41 -0700

    new changes

--- fantasy_app/models.py
+++ fantasy_app/models.py
@@ -1,3 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy import *
 from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
                             backref)
+
+DB = SQLAlchemy()
+
