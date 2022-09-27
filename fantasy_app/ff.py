4569773c50926f812081a33c7795dea686f40ea2
Justin Hsieh <41461549+justin-hsieh@users.noreply.github.com>
2022-09-27 11:44:28 -0700

    Update ff.py

--- fantasy_app/ff.py
+++ fantasy_app/ff.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('..')
-from ..espn_api_submodule.espn_api.football import League
+from fantasy_contests.espn_api_submodule.espn_api.football import League
 #from lists import contests
 
 league = League(
