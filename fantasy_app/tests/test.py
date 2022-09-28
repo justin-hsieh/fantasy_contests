d67f48fe752638b41397dc961c9d0b19685cc6ae
Justin <41461549+justin-hsieh@users.noreply.github.com>
2022-09-28 13:07:47 -0700

    setting up tests

--- fantasy_app/tests/test.py
+++
@@ -1,5 +0,0 @@
-from ..ff import get_yards
-import pytest
-
-def test_yards():
-    assert get_yards(['P'], '139', 1) == {'What does the  Fox Say?': {'player': {'Jack Fox': 185.0, 'Corey Bojorquez': 187.0}}, 'Lets Get  Weird 🐸': {'player': {'Thomas Morstead': 183.0, 'Andy Lee': 255.0}}, 'Team Overson': {'player': {'Arryn Siposs': 137.0, 'Jake Camarda': 152.0}}, 'I h8 justin': {'player': {'Logan Cooke': 150.0, 'Jake Bailey': 118.0}}, 'Punter? I Hardly Know Her': {'player': {'Braden Mann': 253.0, 'Kevin Huber': 158.0}}, '- CircleKirk': {'player': {'Jordan Stout': 291.0, 'Matt Haack': 179.0}}, 'You Hate to See That': {'player': {'Pressley Harvin III': 388.0, 'Jamie Gillan': 309.0}}, 'Vander Eschkimo Bros': {'player': {'Mitch Wishnowsky': 161.0, 'Tommy Townsend': 108.0}}, 'Watsonder My Towel': {'player': {'Sam Martin': 0, "Pat O'Donnell": 179.0}}, 'Ma heart Mahomes': {'player': {'Johnny Hekker': 250.0, 'Michael Dickson': 101.0}}, 'My Chuubbb Hurtz': {'player': {'Bradley Pinion': 180.0, 'Bryan Anger': 254.0}}, "(.)(.) Josh's Allens": {'player': {'Cameron Johnston': 318.0, 'Trenton Gill': 277.0}}}
