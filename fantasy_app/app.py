794e60e1245c742484475e3d7e9c8939d08755b4
Justin <41461549+justin-hsieh@users.noreply.github.com>
2022-11-08 13:06:43 -0800

    codecov addition

--- fantasy_app/app.py
+++ fantasy_app/app.py
@@ -1,49 +1,20 @@
 # Third-party imports
-from flask import Flask, jsonify, request
-import json
+from flask import Flask
+##from decouple import config
 
-#import requests
-#import espn_api_submodule
-#from decouple import config
-from .testing import test_endpoint
-from .ff import get_most_position_points, highest_single_player, order_positions_by_points, get_yards
-from .lists import contests
+
 def create_app():
-        #app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-        app = Flask(__name__)
 
-        # stop tracking modifications
-        #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app = Flask(__name__)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 
-        #DB.init_app(app)
-        @app.route('/')
-        def hello1():
-            return 'Hello, World!!'
+    # stop tracking modifications
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-        @app.route('/test', methods=['GET','POST'])
-        def highest_score():
-     
-            pos = request.get_json()
-            position = pos['contest']
-            points = get_most_position_points(contests[position]['position_list'])
-            points1 = highest_single_player(points)
-            #rank = order_positions_by_points(points)
-            data = json.dumps(points1)
-            return data
+    #DB.init_app(app)
 
-        @app.route('/point', methods=['GET','POST'])
-        def testings():
-            j = request.get_json()
-            position = j['position']
-            response = get_yards(position)
-            data = json.dumps(response)
-
-            return data
-        return app
-
-
-
-'''
-if __name__ == "__main__":
-    app.run(debug=True)
-'''
+    @app.route('/')
+    def hello1():
+        return 'Hello, World!!'
+    
+    return 
--- fantasy_app/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-from sqlalchemy import *
-from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
-                            backref)
-
-DB = SQLAlchemy()
-
