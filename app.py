8527d41715c112f71adb078794968184a03c7e71
Justin Hsieh <41461549+justin-hsieh@users.noreply.github.com>
2023-08-29 15:21:35 -0700

    move app

---
+++ app.py
@@ -0,0 +1,19 @@
+# Third-party imports
+from flask import Flask
+##from decouple import config
+
+
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+# stop tracking modifications
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#DB.init_app(app)
+
+@app.route('/')
+def hello1():
+    return 'Hello, World!!!!f1!'
+
+
