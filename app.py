304fb428fe4ddf71dd791dc9a902deb3a8b94179
Justin Hsieh <41461549+justin-hsieh@users.noreply.github.com>
2024-02-08 10:16:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def calculate_most_points_post():
         contest_list[contest]['position'], contest_list[contest]['stat'], OWNERS, year, week)
     if contest_list[contest].get('single'):
         points = get_highest_points(points)
-    else:
-        points = points
+        
     result_dict['contest_results'] = order_positions_by_points(points)
     result_dict['contest'] = contest
     result_dict['week'] = week
