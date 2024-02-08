5dde768e557977f0f99c74bf181b76af6645c5f4
Justin Hsieh <41461549+justin-hsieh@users.noreply.github.com>
2024-02-08 10:27:05 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -16,9 +16,18 @@ def test_calculate_most_points_get():
 def test_calculate_most_points_post():
     response = app.test_client().post('/most_points_post', 
                                       json={
-                                          "contest": "highest_single_starter_points",
+                                          "contest": "total_lb_tackles",
                                           "week": 14,
                                           "year": "2023"})
+    json_response = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert json_response == {'status':'200', 'data': 'OK'}
+
+    response = app.test_client().post('/most_points_post', 
+                                      json={
+                                          "contest": "highest_single_starter_points",
+                                          "week": 8,
+                                          "year": "2022"})
     json_response = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     assert json_response == {'status':'200', 'data': 'OK'}
